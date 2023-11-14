bb66e5b6a5a89be905c12f8fcb1173966f2aa3d6
adambecker <adambeckercodes@gmail.com>
2023-11-14 17:44:41 +0600

    update

---
+++ js/index.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 40,
+  // loop: true,
+  // centeredSlides: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
